26f36561a7d81bcbb9848078fe0550a6456011d1
Tony Cha <tony.cha@gmail.com>
2014-06-08 21:28:30 -0400

    test pmc

--- web.js
+++ web.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var url = require('url');
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello fool:wq!');
-
+  res.send('Hello fool!');
 });
+
+app.get('/pmc', function(req, res) {
+  console.log(url.parse(req.url))
+}
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {

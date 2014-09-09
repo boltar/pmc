ab7c0b61c84d8ba89e3c3e4b1708ebd5264ac998
Tony Cha <tony.cha@gmail.com>
2014-09-08 21:35:44 -0400

    added feedback for posting

--- web.js
+++ web.js
@@ -5,8 +5,7 @@ var map = require('through2-map')
 var querystring = require ('querystring')
 var bodyParser = require('body-parser')
 var mongoskin = require('mongoskin')
-
-
+var https = require('https');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var mongoUri = process.env.MONGOLAB_URI || 
@@ -80,7 +79,7 @@ function get_pmc(user_name)
 			"name" : "boltar"
 		})
 
-		console.log(pmc);
+		console.log("get_pmc:" + pmc);
 	});
 	return pmc;
 }
@@ -99,6 +98,7 @@ function store_pmc(user_name, time_stamp, pmc, notes)
 			'c' : pmc.slice(2,3),
 			'notes' : notes}, function(err, db) {
 				console.log("inserted " + pmc + " for " + user_name);
+				PostToSlack(user_name + ' posted pmc: ' + pmc);
 			});
 	});
 };
@@ -145,6 +145,25 @@ app.post('/pmc', function(req, res) {
 	})).pipe(res)
 })
 
+// app.post('/getpmc', function(req, res) {
+// 	req.pipe(map(function (chunk) {
+// 		parsed = querystring.parse(chunk.toString())
+// 		console.log(parsed)
+// 		user_name = parsed['user_name'];
+// 		text = parsed['text'];
+// 		timestamp = parsed['timestamp'];
+// 		date = new Date(parseInt(parsed['timestamp']) * 1000)
+
+// 		console.log('user ' + user_name + ' said ' 
+// 			+ text + ' at ' + date.toString());
+
+// 		pmc = get_pmc(user_name); //<<--- this is non-blocking!
+// 		console.log('pmc:' + pmc);
+
+// 		PostToSlack(pmc);
+// 	})).pipe(res)
+// })
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
@@ -152,3 +171,39 @@ app.listen(port, function() {
 });
 
 
+function PostToSlack(post_text) {
+  // Build the post string from an object
+
+  post_data = JSON.stringify(
+  	{"text" : post_text, 
+  	 "username" : "pmcbot",
+  	 "icon_emoji" : ":pmcbot:"
+  	})
+
+  
+  console.log(post_text)
+
+  // An object of options to indicate where to post to
+  var post_options = {
+      host: 'poundc.slack.com',
+      port: '443',
+      path: '/services/hooks/incoming-webhook?token=w0kPrJC0eVqAAnYz7h15yaEh',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': post_data.length
+      }
+  };
+
+  // Set up the request
+  var post_req = https.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('Response: ' + chunk);
+      });
+  });
+  console.log("POST data: " + post_data);
+
+  post_req.write(post_data);
+  post_req.end();
+}
